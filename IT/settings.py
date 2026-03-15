5e8508910544189b5d693e646e425a9d32158f73
ZYH <zyh000202@gmail.com>
2026-03-15 11:33:37 +0800

    Bugs updated

--- IT/settings.py
+++ IT/settings.py
@@ -117,3 +117,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
